3e713935556fe7dd19696c2924a3dd0f4f00c139
Sinan URUN <sinanurun24@gmail.com>
2024-04-20 16:42:05 +0300

    ders12

---
+++ gun01/ders11.py
@@ -0,0 +1,6 @@
+a = 5
+print(a)
+print(type(a))
+print(id(a))
+a = 45
+print(id(a))
---
+++ gun01/ders12.py
@@ -0,0 +1,6 @@
+isim = "Sinan"
+soy_isim = "Urun"
+laptop = True
+boy = 1.94
+plaka = 35
+print(isim, soy_isim, laptop, boy, plaka)
