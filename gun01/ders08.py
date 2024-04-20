3891720098e5e3cab37567d0568c60a06abf0d6a
Sinan URUN <sinanurun24@gmail.com>
2024-04-20 15:43:47 +0300

    Create ders08.py

---
+++ gun01/ders08.py
@@ -0,0 +1,10 @@
+# aritmetiksel operatörler
+print(35 * 9) # * çarpma için kullanılan operatör
+print(35 / 9) # / bölme için kullanılan operatör
+print(35 % 9) # % mod alma için kullanılan operatör
+print(35 + 9)# + toplama için kullanılır
+print(35 - 9)# - çıkartma için kullanılır
+print(35 ** 2)# ** üs alma işlemi için kullanılır
+print(35 // 9)# // tam bölme işlemi için kullanılır
+
+# 
