bb6ce8ea267cafbf87c35fedd585a65c02e8dda4
Sinan URUN <sinanurun24@gmail.com>
2024-04-20 14:31:35 +0300

    g1d3
    g1d3

---
+++ gun01/ders03.py
@@ -0,0 +1,7 @@
+print("Berk")
+print(type("can"))
+# metinsel veriler " " çift tırnak içinde tanımlanır
+# tek - çift ve üç tırnaklı yapılar kullanılabilir
+#bir metin str yani string olarak tanımlanır
+print(8) # sayılar işlemler yapılır
+print("8") # metinsel işlemler yapılır
