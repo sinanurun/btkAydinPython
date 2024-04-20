05114bf1bebf8f888267a61e49aaa6f6eb56cf55
Sinan URUN <sinanurun24@gmail.com>
2024-04-20 14:24:41 +0300

    ders02
    ders02

---
+++ gun01/ders02.py
@@ -0,0 +1,11 @@
+#aşağıda bir tam sayı ekrana yazdırılmıştır
+print(55)
+print(88)
+print(type(48))
+# python dilinde tam sayılar int yani integer olarak tanımlanır
+
+#aşağıda ondalıklı sayı yazdırılmıştır
+print(0.9)
+print(75.45)
+print(type(3.14))
+#python programlama dilinde ondalıklı sayılar float olarak tanımlanır
