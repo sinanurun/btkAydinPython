5ccfd6e019e2eaad967b1485c473ff82c23adc60
Sinan URUN <sinanurun24@gmail.com>
2024-04-20 14:38:52 +0300

    g1d4
    g1d4

---
+++ gun01/ders04.py
@@ -0,0 +1,5 @@
+print(True) # mantıksal veri olarak doğruyu 1 temsil eder
+print(False) # yanlışı yani 0 temsil eder yokluk durumunda kullanaılabilir
+
+print(type(True))
+# mantıksal veri tipi bool yani boolean
