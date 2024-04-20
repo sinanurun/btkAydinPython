e83ac55670edba4336296df4f4a8a23d39183974
Sinan URUN <sinanurun24@gmail.com>
2024-04-20 16:14:17 +0300

    Create ders10.py

---
+++ gun01/ders10.py
@@ -0,0 +1,8 @@
+# mantıksal operatörler
+# karşılaştırma işlemlerini bağlamak amacıyla kullanılır
+# sonuçlar her zaman True veya False olur
+# and ve için, or veya için kullanılır
+print((5 == 5) and ("ali" == "veli"))
+print((5 == 5) or ("ali" == "veli"))
+print(not(5 == 5))
+# not(a) a True ise sonuc False, False ise Sonuc True
