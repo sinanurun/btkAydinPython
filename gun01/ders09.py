1d9194de300add95ca27d73cf08cd4f7783000e3
Sinan URUN <sinanurun24@gmail.com>
2024-04-20 15:59:25 +0300

    Create ders09.py

---
+++ gun01/ders09.py
@@ -0,0 +1,20 @@
+# karşılaştırma operatörleri genelde sayılar üzerinde kullanılır
+# string ve boolean verilerde eşitliği kontrol eder
+# sonuçlar her zman True veya False olur
+print("ali" == "veli")
+print("ali" == "ali")
+# verilerin eşitliği için ==
+
+print("ali" != "veli")
+print("ali" != "ali")
+
+print("8 eşit mi 5'e",8 == 5)
+
+print("23 yaşında ehliyet alınır mı",23 >= 18)
+print("13 yaşında ehliyet alınır mı",13 >= 18)
+
+print("2 yaşında u bilet alınmaz mı",2 <= 3)
+print("5 yaşında u bilet alınmaz mı",5 <= 3)
+
+print(5<6)
+print(5>6)
