5818b8fc8dedfde7a64d5fdcb8f8dd979289bcac
Sinan URUN <sinanurun24@gmail.com>
2024-04-20 17:18:17 +0300

    Create ders13.py

---
+++ gun01/ders13.py
@@ -0,0 +1,14 @@
+#sayısal atama operatörleri
+sayi = 5
+print(sayi)
+sayi += 10 # sayi = sayi + 10
+print(sayi)
+sayi *= 2 # sayi = sayi * 2
+print(sayi)
+
+isim = "Berk"
+print(isim)
+isim += "Demir"
+print(isim)
+isim *= 3
+print(isim)
