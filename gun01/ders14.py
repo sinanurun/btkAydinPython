242e82034d074aa683aa61ea5103e5fb7d0c5dee
Sinan URUN <sinanurun24@gmail.com>
2024-04-20 17:36:00 +0300

    Create ders14.py

---
+++ gun01/ders14.py
@@ -0,0 +1,29 @@
+#tip dönüşümü işlemleri
+sayi = 12
+ssayi = str(sayi)
+print(ssayi*2)
+# x'i stringe çevirmek str(x) kullanılır
+
+isim = "789"
+sayi2 = int(isim)
+print(sayi2)
+
+ondalikli = "12.5"
+osayi = float(ondalikli)
+print(osayi*4)
+
+mantik = ""
+bmantik = bool(mantik)
+print(bmantik)
+
+imantik = 0
+ibool = bool(imantik)
+print(ibool)
+
+bifade = False
+bmetin = str(bifade)
+print(bmetin)
+
+bsayi = False
+isayi = int(bsayi)
+print(isayi)
