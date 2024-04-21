ada88c8230af0a10531d543b6e06af3b7460d32b
Sinan URUN <sinanurun24@gmail.com>
2024-04-21 10:46:27 +0300

    Update ders02.py

--- gun02/ders02.py
+++ gun02/ders02.py
@@ -1,5 +1,6 @@
 # vki programı
-boy = 1.7
-kilo = 60.5
+boy = float(input("Boyunuzu Giriniz : \t"))
+kilo = float(input("Kilonuzu Giriniz : \t"))
 vki = kilo / (boy ** 2)
-
+print(boy,"Boyunda ve",kilo,"kilosunda birinin vki değeri",vki)
+print(f"{boy} Boyunda ve {kilo} kilosunda vki değeri, {vki}")
