bf61549d7d9973aad232d67d5db318d3786508eb
Sinan URUN <sinanurun24@gmail.com>
2024-04-27 12:24:09 +0300

    Create ornek06.py

---
+++ gun03/ornek06.py
@@ -0,0 +1,32 @@
+# bolum 4 soru 20 amstrong sayısı işlemi
+# sayi = int(input("bir sayı giriniz: "))
+# ssayi = str(sayi)
+# toplam = 0
+# for rakam in ssayi:
+#     kup = int(rakam) ** len(ssayi)
+#     toplam += kup
+#     print(rakam,kup,toplam)
+# if toplam == sayi:
+#     print(sayi," sayısı bir amstrong sayısıdır")
+# else:
+#     print(sayi," sayısı bir amstrong sayısı değildir")
+
+#versiyon 2
+# bolum 4 soru 20 amstrong sayısı işlemi
+baslangic = int(input("Baslangic: "))
+bitis = int(input("Bitis: "))
+adet = 0
+for sayi in range(baslangic,bitis):
+    ssayi = str(sayi)
+    toplam = 0
+    for rakam in ssayi:
+        kup = int(rakam) ** len(ssayi)
+        toplam += kup
+        # print(rakam,kup,toplam)
+    if toplam == sayi:
+        print(sayi," sayısı bir amstrong sayısıdır")
+        adet +=1
+    else:
+        continue
+        print(sayi," sayısı bir amstrong sayısı değildir")
+print(f"{baslangic}-{bitis} aralığında {adet} amstrong var")
