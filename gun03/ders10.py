3c1352c73bd8091c7c837b8058eaaaef14e50d9a
Sinan URUN <sinanurun24@gmail.com>
2024-04-27 17:42:14 +0300

    ders12

---
+++ gun03/ders10.py
@@ -0,0 +1,12 @@
+demet = 24,8,2001,"Esra","Gürel"
+print(demet)
+print(type(demet))
+pazar = ["elma","armut","karpuz"]
+pazarlik = tuple(pazar)
+print(pazarlik)
+asayisi = pazarlik.count("armut")
+print(asayisi)
+
+for meyve in pazarlik:
+    print(meyve)
+
---
+++ gun03/ders11.py
@@ -0,0 +1,10 @@
+ogrenciler = {"esra","yılmaz","vahide","hande","yılmaz"}
+print(ogrenciler)
+ogrenciler.pop()
+print(ogrenciler)
+if "yılmaz" in ogrenciler:
+    ogrenciler.remove("yılmaz")
+    print(ogrenciler)
+
+sayilar = {1,5,6,4,2,9,8,5,0,6,3,45,24,10,23,36,52}
+print(sayilar)
