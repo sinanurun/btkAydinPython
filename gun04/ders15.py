0cdd278d22e9f5ca6f7d3412aa09779ea3dfe5eb
Sinan URUN <sinanurun24@gmail.com>
2024-04-28 17:24:30 +0300

    derss

--- gun04/ders15.py
+++ gun04/ders15.py
@@ -12,7 +12,8 @@ sayi1 = int(input("1, sayı"))
 sayi2 = int(input("2, sayı"))
 match islem:
     case "+": print(toplama(sayi1,sayi2))
-    case "-": print(toplama(sayi1,sayi2))
-    case _: print(cikartma(sayi1,sayi2))
+    case "-": print(cikartma(sayi1,sayi2))
+    case "*": print(carpma(sayi1,sayi2))
+    case _: print("Hatalı İşlem Girişi Oldu")
 
 
---
+++ gun04/ders16.py
@@ -0,0 +1,13 @@
+# def daire(r, pi):
+#     alan = pi * r * r
+#     return alan
+#
+# print(daire(4,3.14))
+def kare(a):
+    return a*a
+def daire(r, pi=3.14):
+    alan = pi * kare(r)
+    return alan
+alan = daire(5, 3)
+print(alan)
+print(daire(8))
