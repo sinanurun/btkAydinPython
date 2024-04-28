4a3316da3ef48b54d77fea59474b8f67955e2ca5
Sinan URUN <sinanurun24@gmail.com>
2024-04-28 15:59:44 +0300

    ders09

--- gun04/ders09.py
+++ gun04/ders09.py
@@ -9,7 +9,31 @@ if not (os.path.exists(dosya_adi)):
 while True:
     cevap = int(input("oyun için 1,\tİstatistik için 2,\tÇıkış için 3"))
     if cevap == 1:
-        pass
+        dosya = open(dosya_adi, "a")
+        rastgele = random.randrange(1,100)
+        dosya.write(str(rastgele) + " + ")
+        tahminSayisi = 10
+        taban = 0
+        tavan = 100
+        while tahminSayisi >=1:
+            tahmin = int(input("Bir Sayı Giriniz"))
+            dosya.write(str(tahmin) + ",")
+            if tahmin == rastgele:
+                dosya.write("+ kazandınız \n")
+                print("Tebrikler Kazandınız")
+                dosya.close()
+                break
+            elif tahmin > rastgele:
+                print("daha küçük bir değer giriniz")
+                tavan = tahmin
+            elif tahmin < rastgele:
+                print("daha büyük bir sayı giriniz")
+                taban = tahmin
+            tahminSayisi -= 1
+            print("kalan tahmin sayınız", tahminSayisi)
+            if tahminSayisi == 0:
+                dosya.write("Kaybettiniz \n")
+                dosya.close()
     elif cevap == 2:
         dosya = open(dosya_adi)
         print(dosya.readlines())
