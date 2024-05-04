85decd072a7c44782b5d3333058ad595687d6f60
Sinan URUN <sinanurun24@gmail.com>
2024-05-04 17:39:32 +0300

    Update ders14.py

--- gun05/ders14.py
+++ gun05/ders14.py
@@ -26,6 +26,7 @@ class Otomobil():
             self.hiz += 10
         else:
             print(f" {self.marka} önce aracı çalıştırın")
+        return self.hizGoster()
 
     def hizAzalt(self):
         if self.CalismaDurum == True and self.hiz > 0:
@@ -34,17 +35,47 @@ class Otomobil():
             print(f" {self.marka} araç çalışıyor ama zaten duruyor")
         else:
             print(f" {self.marka} araba zaten çalışmıyor")
+        return self.hizGoster()
+
+    def hizGoster(self):
+        if self.CalismaDurum == False:
+            self.hiz = 0
+        print(f" {self.marka} aracın hızı {self.hiz}")
+        return self.hiz
+
+    def cikis(self):
+        print("Çıkış yapıldı")
+        exit()
 
     def __str__(self):
         return f'{self.marka}, {self.model} Otomobil'
 
 
 oto1 = Otomobil("Ford","Focus","Gri")
-oto1.calistir()
-oto1.calistir()
-print(oto1)
-oto2 = Otomobil("Audi","A4","Kırmızı")
-print(oto2)
-print(oto2.CalismaDurum)
-oto2.calistir()
+# oto1.calistir()
+# oto1.calistir()
+# print(oto1)
+# oto2 = Otomobil("Audi","A4","Kırmızı")
+# print(oto2)
+# print(oto2.CalismaDurum)
+# oto2.calistir()
+# oto2.hizArttir()
+# oto2.hizGoster()
+# oto2.hizArttir()
 
+while True:
+    cevap = int(input("Çalıştır : 1 \t Durdur: 2\n, arttır: 3\t, Azatl : 4\n Hız Göster : 5\t, Çıkış 6 \n"))
+    if cevap == 1:
+        oto1.calistir()
+    elif cevap == 2:
+        oto1.durdur()
+    elif cevap == 3:
+        oto1.hizArttir()
+    elif cevap == 4:
+        oto1.hizAzalt()
+    elif cevap == 5:
+        oto1.hizGoster()
+    elif cevap == 6:
+        oto1.cikis()
+    else:
+        print("hatalı işlem girişi")
