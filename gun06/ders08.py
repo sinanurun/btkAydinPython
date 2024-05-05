118e1460fcd42dea1a63eb3a5e6cff248b9f68c4
Sinan URUN <sinanurun24@gmail.com>
2024-05-05 16:14:56 +0300

    Update ders08.py

--- gun06/ders08.py
+++ gun06/ders08.py
@@ -1,5 +1,6 @@
 import json
 kitap_listesi = []
+kitaplar = []
 class Kitap():
     def __init__(self,ad,yazar,sayfa,basim):
         self.ad=ad
@@ -7,14 +8,29 @@ class Kitap():
         self.sayfa=sayfa
         self.basim=basim
 
-def kitapListesineEkle(kitap):
+def kitapListesineEkle():
+    kitap_adi = input("Kitap adı")
+    kitap_yazar = input("yazar")
+    kitap_sayfa = int(input("sayfa"))
+    kitap_yili = int(input("yili"))
+    kitap = Kitap(kitap_adi,kitap_yazar, kitap_sayfa, kitap_yili)
     degerler = vars(kitap)
     kitap_listesi.append(degerler)
+    kitaplar.append(kitap)
 
 def kitapListesiKaydet():
     y = json.dumps(kitap_listesi)
     with open("kitap.json","w") as file:
         file.write(y)
+
+def menu():
+    k_giris = input("Kaydet, Ekle, Çıkart, Listele, Q")
+    return k_giris
+while True:
+    cevap = menu()
+    if cevap == "E":
+
+
 
 # kitap = Kitap("Python","Sinan Hoca",30, 2024)
 #
