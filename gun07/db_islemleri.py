3c62859ce3a02c45fbdcd986ac29d02a282c752e
Sinan URUN <sinanurun24@gmail.com>
2024-05-11 14:08:57 +0300

    aSAs

--- gun07/db_islemleri.py
+++ gun07/db_islemleri.py
@@ -8,4 +8,13 @@ def birim_listele():
     birimler = session.query(Birim).all()
     print(birimler)
 
+def birim_ekle():
+    b_adi = input("Eklenecek Birim Adını Giriniz")
+    yeni_birim = Birim(birim_adi=b_adi)
+    session.add(yeni_birim)
+    session.commit()
+
 birim_listele()
+birim_ekle()
+birim_listele()
+
