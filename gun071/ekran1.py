4a1af02fe0e3fd9ff962652bfe5c882c6e73c796
Sinan URUN <sinanurun24@gmail.com>
2024-05-11 16:36:24 +0300

    Update ekran1.py

--- gun071/ekran1.py
+++ gun071/ekran1.py
@@ -7,6 +7,13 @@ class AnaEkranPenceresi(QWidget, Ui_Form):
         super().__init__()
         self.setupUi(self)
 
+        self.pushButton.clicked.connect(self.arttir)
+
+    def arttir(self):
+        sayi = int(self.label.text())
+        sayi +=1
+        self.label.setText(str(sayi))
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     pencere = AnaEkranPenceresi()
