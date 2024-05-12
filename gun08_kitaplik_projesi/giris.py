5e7d1f29e502dfb6978eaa4677bb519c374d1201
Sinan URUN <sinanurun24@gmail.com>
2024-05-12 10:46:13 +0300

    Update giris.py

--- gun08_kitaplik_projesi/giris.py
+++ gun08_kitaplik_projesi/giris.py
@@ -11,10 +11,21 @@
 from PyQt5.QtWidgets import *
 import sys
 from girisui import Ui_Giris_Formu
+import db_islemleri
 class Giris_Formu(QWidget, Ui_Giris_Formu):
     def __init__(self):
         super(Giris_Formu, self).__init__()
         self.setupUi(self)
+        self.girisButonu.clicked.connect(self.giris_kontrol)
+
+    def giris_kontrol(self):
+        k_adi = self.kAdiEdit.text()
+        k_sifre =self.kSifreEdit.text()
+        user = db_islemleri.k_girisi(k_adi,k_sifre)
+        if user != 0 :
+            print("oturum açan kullanıcı idsi:",user)
+        else:
+            print("oturum açma işlemi gerçekleşmedi")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
